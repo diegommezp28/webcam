54405098fc049c4c6dad48d32a0ffcad9bcb4db8
yushuhuang <huangyushu613@gmail.com>
2019-04-18 18:29:27 -0400

    minor changes

--- receive.py
+++ receive.py
@@ -8,27 +8,29 @@ buf = 512
 width = 640
 height = 480
 code = b'start'
-l = width * height * 3
+num_of_chunks = width * height * 3 / buf
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(addr)
     while True:
-        byte_frame = bytearray()
+        chunks = []
         start = False
-        while (len(byte_frame) < l):
-            data, _ = s.recvfrom(buf)
+        while len(chunks) < num_of_chunks:
+            chunk, _ = s.recvfrom(buf)
             if start:
-                byte_frame.extend(data)
-            elif data.startswith(code):
+                chunks.append(chunk)
+            elif chunk.startswith(code):
                 start = True
+
+        byte_frame = b''.join(chunks)
 
         frame = np.frombuffer(
             byte_frame, dtype=np.uint8).reshape(height, width, 3)
 
         cv.imshow('recv', frame)
         if cv.waitKey(1) & 0xFF == ord('q'):
-            s.close()
             break
 
+    s.close()
     cv.destroyAllWindows()
--- send.py
+++ send.py
@@ -14,28 +14,20 @@ code = 'start'
 code = ('start' + (buf - len(code)) * 'a').encode('utf-8')
 
 
-def sendFrame(frame):
+if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.sendto(code, addr)
-    data = frame.tostring()
-    for i in range(0, len(data), buf):
-        s.sendto(data[i:i+buf], addr)
-    s.close()
-
-
-def captureFunc():
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret:
-            cv.imshow('send', frame)
-            sendFrame(frame)
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
+            s.sendto(code, addr)
+            data = frame.tostring()
+            for i in range(0, len(data), buf):
+                s.sendto(data[i:i+buf], addr)
+            # cv.imshow('send', frame)
+            # if cv.waitKey(1) & 0xFF == ord('q'):
+                # break
         else:
             break
-
-
-if __name__ == '__main__':
-    captureFunc()
-    cap.release()
-    cv.destroyAllWindows()
+    # s.close()
+    # cap.release()
+    # cv.destroyAllWindows()
